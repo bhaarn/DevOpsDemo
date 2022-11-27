153fb66cba024b4e315a163f254c999ca8ee8ca2
bhaarn <92562462+bhaarneval@users.noreply.github.com>
2022-11-27 09:53:01 +0530

    Added Release files and Shell file

---
+++ build_process.sh
@@ -0,0 +1,6 @@
+cd /Users/bhaarn/Documents/Personal/lecture/BITS/Code/Flutter/DevOpsDemo/android
+./gradlew assembleDebug
+./gradlew test
+./gradlew lint
+/Users/bhaarn/Library/Android/sdk/platform-tools/adb install /Users/bhaarn/Documents/Personal/lecture/BITS/Code/Flutter/DevOpsDemo/app/build/outputs/apk/debug/app-debug.apk
+/Users/bhaarn/Library/Android/sdk/platform-tools/adb shell am start -n com.padhuga.devops/com.padhuga.devops.MainActivity
